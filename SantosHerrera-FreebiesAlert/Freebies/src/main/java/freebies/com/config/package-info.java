e8b4ee1c96f67397a0050e4a5ddff0aacfe59db9
santos <santoslopez890@gmail.com>
2023-08-23 16:51:53 -0400

    added stuff

--- SantosHerrera-FreebiesAlert/Freebies/src/main/java/freebies/com/config/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Framework configuration files.
- */
-package freebies.com.config;
--- SantosHerrera-FreebiesAlert/Freebies/src/main/java/freebies/com/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package freebies.com.repository;
-
-import freebies.com.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
