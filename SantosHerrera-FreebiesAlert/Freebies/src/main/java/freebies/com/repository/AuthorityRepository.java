1ca339d788eeff634c5bb823cc44d7a3dbbd8849
santos <santoslopez890@gmail.com>
2023-08-23 17:14:06 -0400

    Revert "added stuff"
    This reverts commit e8b4ee1c96f67397a0050e4a5ddff0aacfe59db9.

---
+++ SantosHerrera-FreebiesAlert/Freebies/src/main/java/freebies/com/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package freebies.com.repository;
+
+import freebies.com.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ SantosHerrera-FreebiesAlert/Freebies/src/main/java/freebies/com/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package freebies.com.security;
